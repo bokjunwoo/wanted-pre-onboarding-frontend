fa0bf0f7a5272e7dcc788539624508551a8974a2
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-02-03 15:07:23 +0900

    feat : 투두 폼 기능 구현

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Form, Button, Input, Space } from 'antd';
+import { useCallback } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-export default function TodoForm() {
+export default function TodoForm({ token, render, setRender }) {
+  // 투두
+  const [todo, setTodo] = useState('')
+  const onChangeTodo = useCallback((e) => {
+    setTodo(e.target.value)
+  }, [])
+
+  // 투두 작성 폼
+  const onSubmitTodoFrom = useCallback(() => {
+    axios.post('http://localhost:8000/todos', { todo }, { headers: { Authorization: `Bearer ${token}` } })
+      .then(() => {
+        alert('새로운 TODO가 입력되었습니다.')
+        setTodo('')
+        setRender(!render)
+      }).catch(() => {
+        alert('로그인이 필요합니다.')
+      })
+  }, [todo, token, render, setRender])
+
   return (
-    <Form>
+    <Form onFinish={onSubmitTodoFrom}>
       <h3>TODO를 적어주세요</h3>
       <Space>
-        <Input type='text' data-testid="new-todo-input" />
-        <Button data-testid="new-todo-add-button">추가</Button>
+        <Input type='text' data-testid="new-todo-input" value={todo} onChange={onChangeTodo} required />
+        <Button htmlType='submit' type='primary' data-testid="new-todo-add-button">추가</Button>
       </Space>
     </Form>
   )
