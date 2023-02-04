3482c377e4e8048fd7ee68ebd7e497385227ddf5
JUNU <bokjunwoo@gmail.com>
2023-02-04 23:35:35 +0900

    feat : Index 페이지

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
 
 export default function Index() {
+  const ParentBox = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `
+  const Box = styled.div`
+    width: 200px;
+    height: 100px;
+    border: 2px solid black;
+    text-align: center;
+    font-size: 50px;
+    line-height: 100px;
+    margin: 10px;
+    box-sizing: border-box;
+    border-radius: 10px;
+  `
+  const LinkTo = styled(Link)`
+    text-decoration: none;
+    color: black;
+  `
   return (
-    <div>Index</div>
+    <ParentBox>
+      <LinkTo to='/signin'><Box>로그인</Box></LinkTo>
+      <LinkTo to='/signup'><Box>회원가입</Box></LinkTo>
+      <LinkTo to='/todo'><Box>TODO</Box></LinkTo>
+    </ParentBox>
   )
 }
